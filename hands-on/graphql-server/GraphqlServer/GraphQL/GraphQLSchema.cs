3d8c7338c0d0c1346d46fa8496ae323dd4738189
Rodrigo Miranda Terra <rodrigo.terra@gmail.com>
2019-03-09 10:43:29 -0300

    Criando arquivo com o material de referência

---
+++ hands-on/graphql-server/GraphqlServer/GraphQL/GraphQLSchema.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GraphqlServer.GraphQL
+{
+    public class GraphQLSchema
+    {
+    }
+}
